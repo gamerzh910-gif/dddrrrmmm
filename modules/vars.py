2ca1742f07c9f447ec1191e5f118fe0aed544c34
gamerzh910-gif <gamerzh910@gmail.com>
2025-11-25 21:14:54 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,18 +3,18 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "29899250"))
-API_HASH = environ.get("API_HASH", "611d045796c79af3e5ddfa3d6fd536a7")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8318959084:AAFyQC0nit-8yx6EQiRV2-R2AaxHSOMkibs")
+API_ID = int(environ.get("API_ID", "39097403"))
+API_HASH = environ.get("API_HASH", "c0a7c90594ca10dd347bd4a2e69ba3ff")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7679116445:AAEf8xwhROdnvsoJZ4bDhPLObn3351WJfEI")
 
-OWNER = int(environ.get("OWNER", "6053889491"))
-CREDIT = environ.get("CREDIT", "@ll_kumawat_ll")
+OWNER = int(environ.get("OWNER", "8140993079"))
+CREDIT = environ.get("CREDIT", "@leavingproperty")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '6053889491').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '8140993079').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '6053889491').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '814099307').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
